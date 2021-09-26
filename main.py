b7f5cf44bcf3a6e71795a84fe0ff4096490646d1
rus-ai <russian-ai@yandex.ru>
2021-09-26 22:16:23 +0300

    Move stylesheet to separate file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5 import QtWidgets, uic
 from faker import Faker
 
 
-fake = Faker(['ru-RU'])
+fake = Faker(['ru-RU', 'en_US'])
 
 
 class ContactItem(QtWidgets.QWidget):
@@ -14,14 +14,18 @@ class ContactItem(QtWidgets.QWidget):
         namesWord = contactName.split(" ")
         self.nameLabel.setText(contactName)
         self.lastmessageLabel.setText(lastMessageText)
-        self.avatarLabel.setText(namesWord[0][0]+namesWord[1][0])
-        self.avatarLabel.setStyleSheet(self.avatarLabel.styleSheet()+f"background-color: rgb({randrange(200)},{randrange(200)},{randrange(200)}); ")
+        self.ContactAvatar.setText(namesWord[0][0]+namesWord[1][0])
+        self.ContactAvatar.setStyleSheet(f"background-color: rgb({randrange(200)},{randrange(200)},{randrange(200)}); ")
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi(path.join("ui", "ChatMain.ui"), self)
+        stileFile = open(path.join("ui", "ChatMain.css"), 'r')
+        self.styleData = stileFile.read()
+        stileFile.close()
+        self.ui = uic.loadUi(path.join("ui", "ChatMain.ui"), self)
+        self.ui.setStyleSheet(self.styleData)
         for count in range(29):
             self.ContactsLayout.addWidget(ContactItem(fake.name(), "..."))
 
