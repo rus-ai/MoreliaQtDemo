ab9d36be28fe96cd658216677c03af637232cd7f
rus-ai <russian-ai@yandex.ru>
2021-09-27 22:35:34 +0300

    Chat messages demo

--- main.py
+++ main.py
@@ -18,6 +18,20 @@ class ContactItem(QtWidgets.QWidget):
         self.ContactAvatar.setStyleSheet(f"background-color: rgb({randrange(200)},{randrange(200)},{randrange(200)}); ")
 
 
+class MessageItem(QtWidgets.QWidget):
+    def __init__(self, contactName, MessageText, MessageTime):
+        super(MessageItem, self).__init__()
+        uic.loadUi(path.join("ui", "MessageItem.ui"), self)
+        userColor = f"rgb({randrange(200)},{randrange(200)},{randrange(200)}); "
+        namesWord = contactName.split(" ")
+        self.usernameLabel.setText(contactName)
+        self.usernameLabel.setStyleSheet("color: "+userColor)
+        self.messageLabel.setText(MessageText)
+        self.timeLabel.setText(MessageTime)
+        self.ContactAvatar.setText(namesWord[0][0]+namesWord[1][0])
+        self.ContactAvatar.setStyleSheet("background-color: "+userColor)
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -28,6 +42,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setStyleSheet(self.styleData)
         for count in range(29):
             self.ContactsLayout.addWidget(ContactItem(fake.name(), "..."))
+        for count in range(5):
+            self.MessageLayout.addWidget(MessageItem(fake.name(), fake.text(), f"00:0{count}"))
 
 
 if __name__ == "__main__":
